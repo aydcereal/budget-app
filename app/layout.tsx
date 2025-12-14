7fb6137b6fe6dfcf08145968084da579a46dfc48
aydcereal <jordyfigueroa93@icloud.com>
2025-12-14 14:29:53 -0800

    sidebar created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="flex min-h-screen">
+          <Sidebar/>
+          <main className="flex-1 bg-gray-100 p-6">
+            
+          </main>
+        </div>
         {children}
       </body>
     </html>
