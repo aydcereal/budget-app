b1ef743c480881be2a6c7f6e1a730c9fef64e842
aydcereal <jordyfigueroa93@icloud.com>
2025-12-14 14:30:41 -0800

    sidebar**

---
+++ components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Sidebar(){
+    return(
+        <aside className="w-64 bg-slate-900 text-white p-6 space-y-4">
+            <h1 className="text-x1 font-bold mb-6">DebtBuddy</h1>
+
+        </aside>
+    );
+        
+    
+    
+}
